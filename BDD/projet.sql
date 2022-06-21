f8eb083bf3dd75cf58b74fcad92c01614bbbed8d
TrLSeysh <trlseysh@gmail.com>
2022-06-21 16:08:11 +0200

    Update projet.sql

--- BDD/projet.sql
+++ BDD/projet.sql
@@ -93,7 +93,7 @@ FLUSH PRIVILEGES;
 
 INSERT INTO sports (name, picture) VALUES ('soccer', "/assets/sports/soccer.png");
 INSERT INTO sports (name, picture) VALUES ('baseball', "/assets/sports/baseball.png");
-INSERT INTO sports (name, picture) VALUES ('basketball', "/assets/sports/basketball.png");
+INSERT INTO sports (name, picture) VALUES ('basketball', "/assets/sports/basket.png");
 INSERT INTO sports (name, picture) VALUES ('football', "/assets/sports/football.png");
 INSERT INTO sports (name, picture) VALUES ('badminton', "/assets/sports/goodminton.png");
 INSERT INTO sports (name, picture) VALUES ('swimming', "/assets/sports/swimming.png");
